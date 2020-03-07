da3420c9d38be2c9210b01bd4ca921991410f935
Aggr3ssor <33804388+Aggr3ssor@users.noreply.github.com>
2020-03-08 03:11:22 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import requests
+import json
+import datetime
+import random
+import string
+import os
+import time 
+
+referrer = input("USER ID (Setting/More settings/Diagnostics/ID): ")
+
+def genString(stringLength):
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+ver = 'v0a745'
+url = f'https://api.cloudflareclient.com/{ver}/reg'
+
+
+def run():
+    install_id = genString(11)
+    body = {"key": "{}=".format(genString(42)),
+            "install_id": install_id,
+            "fcm_token": f"{install_id}:APA91b{genString(134)}",
+            "referrer": referrer,
+            "warp_enabled": False,
+            "tos": datetime.datetime.now().isoformat()[:-3] + "+07:00",
+            "type": "Android",
+            "locale": "zh-CN"}
+
+    bodyString = json.dumps(body)
+
+    headers = {'Content-Type': 'application/json; charset=UTF-8',
+               'Host': 'api.cloudflareclient.com',
+               'Connection': 'Keep-Alive',
+               'Accept-Encoding': 'gzip',
+               'User-Agent': 'okhttp/3.12.1'
+               }
+
+    r = requests.post(url, data=bodyString, headers=headers)
+    return r
+    
+
+
+while True:
+    result = run()
+    if result.status_code == 200:
+        print("+1 GB")
+        time.sleep(15)
